e06736f299f9a44344a8f7d7f3ba7c0284798ac7
sm730bf <mesziist@gmail.com>
2022-09-14 14:20:37 +0200

    a

--- tmp.c
+++ tmp.c
@@ -2,7 +2,13 @@
 #include <stdlib.h>
 
 int main() {
+    double a;
+    int b;
+    double result;
+    
+    
     printf("\nPélda");
+    
     printf("\nSzámítás: %f / %d = %.2f", 10.0, 3, 10.0/3);
     return 0;
 }
